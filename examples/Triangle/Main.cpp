76a9fb42f590631e75ef7316b94c718dc21d0f80
Hernan Saez <hhsaez@gmail.com>
2019-09-30 09:00:02 -0300

    Update vulkan examples

--- examples/Triangle/Main.cpp
+++ examples/Triangle/Main.cpp
@@ -9,14 +9,14 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of the copyright holder nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,17 +26,22 @@
  */
 
 #include <Crimild.hpp>
-#include <Crimild_SDL.hpp>
+#include <Crimild_Vulkan.hpp>
+#include <Crimild_GLFW.hpp>
 
 using namespace crimild;
+using namespace crimild::glfw;
+using namespace crimild::vulkan;
 using namespace crimild::rendergraph;
 
 int main( int argc, char **argv )
 {
 	crimild::init();
 	
-    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< sdl::SDLSimulation >( "Triangle", crimild::alloc< Settings >( argc, argv ) );
+    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Triangle", crimild::alloc< Settings >( argc, argv ) );
+	sim->addSystem( crimild::alloc< GLFWVulkanSystem >() );
 
+	/*
     auto scene = crimild::alloc< Group >();
 
     float vertices[] = {
@@ -68,6 +73,7 @@ int main( int argc, char **argv )
     scene->attachNode( camera );
     
     sim->setScene( scene );
+	*/
 	return sim->run();
 }
 
