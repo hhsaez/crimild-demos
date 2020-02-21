0b8fbed0d401062d89420a86191ac1d9e65c2f5c
Hernan Saez <hhsaez@gmail.com>
2020-02-21 16:54:08 -0300

    Update Skybox example

--- examples/Skybox/Main.cpp
+++ examples/Skybox/Main.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2002-present, H. Hernan Saez
+ * Copyright (c) 2002 - present, H. Hernan Saez
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -9,14 +9,14 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of the copyright holder nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -25,80 +25,127 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define CRIMILD_ENABLE_CHECK_GL_ERRORS 1
-
 #include <Crimild.hpp>
-#include <Crimild_SDL.hpp>
+#include <Crimild_Vulkan.hpp>
+#include <Crimild_GLFW.hpp>
+#include <Crimild_STB.hpp>
 
 using namespace crimild;
-using namespace crimild::sdl;
-using namespace crimild::rendergraph;
-using namespace crimild::rendergraph::passes;
+using namespace crimild::glfw;
 
-SharedPointer< RenderGraph > createRenderGraph( void )
-{
-	auto graph = crimild::alloc< RenderGraph >();
+class ExampleVulkanSystem : public GLFWVulkanSystem {
+public:
+    crimild::Bool start( void ) override
+    {
+        if ( !GLFWVulkanSystem::start() ) {
+            return false;
+        }
 
-	auto depthPass = graph->createPass< DepthPass >();
-	auto scenePass = graph->createPass< ForwardLightingPass >();
-	auto skyboxPass = graph->createPass< SkyboxPass >();
-	auto blend = graph->createPass< BlendPass >();
+        m_scene = [&] {
+            auto scene = crimild::alloc< Group >();
 
-    scenePass->setDepthInput( depthPass->getDepthOutput() );
-    skyboxPass->setDepthInput( depthPass->getDepthOutput() );
+            // Skybox must go first for the moment
+            scene->attachNode( [] {
+				return crimild::alloc< Skybox >(
+                    containers::Array< SharedPointer< Image >> {
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/right.tga" } ),
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/left.tga" } ),
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/top.tga" } ),
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/bottom.tga" } ),
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/back.tga" } ),
+                        crimild::ImageManager::getInstance()->loadImage( { .filePath = "assets/textures/front.tga" } ),
+                    }
+                );
+            }());
 
-    blend->addInput( scenePass->getColorOutput() );
-    blend->addInput( skyboxPass->getColorOutput() );
+            scene->attachNode( [] {
+                auto path = FilePath {
+                    .path = "assets/models/bunny/bunny.obj",
+                };
+                auto group = crimild::alloc< Group >();
+                OBJLoader loader( path.getAbsolutePath() );
+                if ( auto model = loader.load() ) {
+                    group->attachNode( model );
+                }
+                group->attachComponent< RotationComponent >( Vector3f::UNIT_Y, 0.1f );
+                return group;
+            }());
 
-    graph->setOutput( blend->getOutput() );
+            scene->attachNode([] {
+                auto settings = Simulation::getInstance()->getSettings();
+                auto width = settings->get< crimild::Real32 >( "video.width", 0 );
+                auto height = settings->get< crimild::Real32 >( "video.height", 1 );
+                auto camera = crimild::alloc< Camera >( 45.0f, width / height, 0.1f, 100.0f );
+                camera->local().setTranslate( 0.0f, 0.0f, 10.0f );
+                camera->attachComponent< FreeLookCameraComponent >();
+                Camera::setMainCamera( camera );
+                return camera;
+            }());
 
-	return graph;
-}
+            return scene;
+        }();
+
+        m_scene->perform( StartComponents() );
+
+        auto commandBuffer = [ this ] {
+            auto commandBuffer = crimild::alloc< CommandBuffer >();
+
+            commandBuffer->begin( CommandBuffer::Usage::SIMULTANEOUS_USE );
+            commandBuffer->beginRenderPass( nullptr );
+
+            m_scene->perform( Apply( [ commandBuffer ]( Node *node ) {
+                if ( auto renderState = node->getComponent< RenderStateComponent >() ) {
+                    renderState->commandRecorder( crimild::get_ptr( commandBuffer ) );
+                }
+            }));
+
+            commandBuffer->endRenderPass( nullptr );
+            commandBuffer->end();
+
+            return commandBuffer;
+        }();
+
+        setCommandBuffers( { commandBuffer } );
+
+        return true;
+    }
+
+    void update( void ) override
+    {
+        auto clock = Simulation::getInstance()->getSimulationClock();
+        m_scene->perform( UpdateComponents( clock ) );
+        m_scene->perform( UpdateWorldState() );
+
+        GLFWVulkanSystem::update();
+    }
+
+    void stop( void ) override
+    {
+        if ( auto renderDevice = getRenderDevice() ) {
+            renderDevice->waitIdle();
+        }
+
+        m_scene = nullptr;
+
+        GLFWVulkanSystem::stop();
+    }
+
+private:
+    SharedPointer< Node > m_scene;
+};
 
 int main( int argc, char **argv )
 {
-	auto sim = crimild::alloc< SDLSimulation >( "Skybox", crimild::alloc< Settings >( argc, argv ) );
+    crimild::init();
+    crimild::vulkan::init();
 
-	auto scene = crimild::alloc< Group >();
+    Log::setLevel( Log::Level::LOG_LEVEL_ALL );
 
-	auto box = crimild::alloc< Geometry >();
-	box->attachPrimitive( crimild::alloc< BoxPrimitive >( 1.0f, 1.0f, 1.0f ) );
-	auto boxMaterial = crimild::alloc< Material >();
-	boxMaterial->setAmbient( RGBAColorf::ONE );
-	box->getComponent< MaterialComponent >()->attachMaterial( boxMaterial );
-	scene->attachNode( box );
+    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Skybox", crimild::alloc< Settings >( argc, argv ) );
 
-	auto sun = crimild::alloc< Light >( Light::Type::DIRECTIONAL );
-	sun->local().rotate().fromEulerAngles( -0.25f * Numericf::PI, -0.75f * Numericf::PI, 0.0f );
-	scene->attachNode( sun );
+    SharedPointer< ImageManager > imageManager = crimild::alloc< crimild::stb::ImageManager >();
 
-	auto ambient = crimild::alloc< Light >( Light::Type::AMBIENT );
-    ambient->setAmbient( RGBAColorf( 0.1f, 0.1f, 0.25f, 1.0f ) );
-	scene->attachNode( ambient );
-
-#if 1
-	scene->attachNode(
-		crimild::alloc< Skybox >(
-			containers::Array< SharedPointer< Image >> {
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/right.tga" ) ),
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/left.tga" ) ),
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/top.tga" ) ),
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/bottom.tga" ) ),
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/back.tga" ) ),
-				crimild::alloc< ImageTGA >( FileSystem::getInstance().pathForResource( "assets/textures/front.tga" ) ),
-			}
-		)
-	);
-#endif
-
-	auto camera = crimild::alloc< Camera >();
-	camera->local().setTranslate( 0.f, 0.0f, 5.0f );
-	camera->attachComponent< FreeLookCameraComponent >();
-    camera->setRenderGraph( createRenderGraph() );
-	scene->attachNode( camera );
-
-	sim->setScene( scene );
-
-	return sim->run();
+    sim->addSystem( crimild::alloc< ExampleVulkanSystem >() );
+    return sim->run();
 }
 
