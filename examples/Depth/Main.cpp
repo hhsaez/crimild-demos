7370fa068d7892df5c4723892ad50bd53c91ae1a
Hernan Saez <hhsaez@gmail.com>
2020-02-28 11:52:28 -0300

    Update Depth example

--- examples/Depth/Main.cpp
+++ examples/Depth/Main.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2013, Hernan Saez
+ * Copyright (c) 2002 - present, H. Hernan Saez
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -9,14 +9,14 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of the copyright holder nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,50 +26,163 @@
  */
 
 #include <Crimild.hpp>
-#include <Crimild_SDL.hpp>
-
-#include "Rendering/RenderGraph/RenderGraph.hpp"
-#include "Rendering/RenderGraph/Passes/DepthPass.hpp"
-#include "Rendering/RenderGraph/Passes/LinearizeDepthPass.hpp"
+#include <Crimild_Vulkan.hpp>
+#include <Crimild_GLFW.hpp>
+#include <Crimild_STB.hpp>
 
 using namespace crimild;
-using namespace crimild::rendergraph;
-using namespace crimild::sdl;
+using namespace crimild::glfw;
 
-SharedPointer< RenderGraph > createRenderGraph( void )
-{
-	auto graph = crimild::alloc< RenderGraph >();
+class ExampleVulkanSystem : public GLFWVulkanSystem {
+public:
+    crimild::Bool start( void ) override
+    {
+        if ( !GLFWVulkanSystem::start() ) {
+            return false;
+        }
 
-	auto depthPass = graph->createPass< passes::DepthPass >();
-    auto linearizeDepthPass = graph->createPass< passes::LinearizeDepthPass >();
+        m_scene = [&] {
+            auto scene = crimild::alloc< Group >();
 
-	linearizeDepthPass->setInput( depthPass->getDepthOutput() );	
+            scene->attachNode( [] {
+                auto path = FilePath {
+                    .path = "assets/models/sponza/sponza.obj",
+                };
+                auto group = crimild::alloc< Group >();
+                OBJLoader loader( path.getAbsolutePath() );
+                loader.pipeline = [&] {
+                    auto pipeline = crimild::alloc< Pipeline >();
+                    pipeline->program = [&] {
+                        auto program = crimild::alloc< ShaderProgram >(
+                            containers::Array< SharedPointer< Shader >> {
+                                crimild::alloc< Shader >(
+                                    Shader::Stage::VERTEX,
+                                    FileSystem::getInstance().readFile(
+                                        FilePath {
+                                            .path = "assets/shaders/depth.vert.spv"
+                                        }.getAbsolutePath()
+                                    )
+                                ),
+                                crimild::alloc< Shader >(
+                                    Shader::Stage::FRAGMENT,
+                                    FileSystem::getInstance().readFile(
+                                        FilePath {
+                                            .path = "assets/shaders/depth.frag.spv"
+                                        }.getAbsolutePath()
+                                    )
+                                ),
+                            }
+                        );
+                        program->attributeDescriptions = VertexP3N3TC2::getAttributeDescriptions( 0 );
+                        program->bindingDescription = VertexP3N3TC2::getBindingDescription( 0 );
+                        program->descriptorSetLayout = [] {
+                            auto layout = crimild::alloc< DescriptorSetLayout >();
+                            layout->bindings = {
+                                {
+                                    .descriptorType = DescriptorType::UNIFORM_BUFFER,
+                                    .stage = Shader::Stage::VERTEX,
+                                },
+                            };
+                            return layout;
+                        }();
+                        return program;
+                    }();
+//                    pipeline->polygonState = PolygonState::LINE;
+                    return pipeline;
+                }();
+                if ( auto model = loader.load() ) {
+                    group->attachNode( model );
+                    model->perform( Apply( [&]( Node *node ) {
+                        if ( auto rs = node->getComponent< RenderStateComponent >() ) {
+                            rs->uniforms = {
+                                [&] {
+                                    auto uniform = crimild::alloc< ModelViewProjectionUniformBuffer >();
+                                    uniform->node = crimild::get_ptr( model );
+                                    return uniform;
+                                }(),
+                            };
+                            rs->textures = { };
+                        }
+                    }));
+                    model->local().setScale( 0.1f );
+                }
+                return group;
+            }());
 
-	graph->setOutput( linearizeDepthPass->getOutput() );
+            scene->attachNode([] {
+                auto camera = crimild::alloc< Camera >( 45.0f, 4.0f / 3.0f, 0.1f, 500.0f );
+                camera->local().setTranslate( -103.914337f, 7.80385065f, -3.4776721f );
+                camera->local().setRotate( Quaternion4f( 0.0996486619f, -0.701276481f, 0.10000769f, 0.698762059f ) );
+                camera->attachComponent< FreeLookCameraComponent >();
+                Camera::setMainCamera( camera );
+                return camera;
+            }());
 
-	return graph;
-}
+            scene->perform( StartComponents() );
+
+            return scene;
+        }();
+
+        auto commandBuffer = [ this ] {
+            auto commandBuffer = crimild::alloc< CommandBuffer >();
+
+            commandBuffer->begin( CommandBuffer::Usage::SIMULTANEOUS_USE );
+            commandBuffer->beginRenderPass( nullptr );
+
+            m_scene->perform( Apply( [ commandBuffer ]( Node *node ) {
+                if ( auto renderState = node->getComponent< RenderStateComponent >() ) {
+                    renderState->commandRecorder( crimild::get_ptr( commandBuffer ) );
+                }
+            }));
+
+            commandBuffer->endRenderPass( nullptr );
+            commandBuffer->end();
+
+            return commandBuffer;
+        }();
+
+        setCommandBuffers( { commandBuffer } );
+
+        return true;
+    }
+
+    void update( void ) override
+    {
+        auto clock = Simulation::getInstance()->getSimulationClock();
+        m_scene->perform( UpdateComponents( clock ) );
+        m_scene->perform( UpdateWorldState() );
+
+        GLFWVulkanSystem::update();
+    }
+
+    void stop( void ) override
+    {
+        if ( auto renderDevice = getRenderDevice() ) {
+            renderDevice->waitIdle();
+        }
+
+        m_scene = nullptr;
+
+        GLFWVulkanSystem::stop();
+    }
+
+private:
+    SharedPointer< Node > m_scene;
+};
 
 int main( int argc, char **argv )
 {
-	auto sim = crimild::alloc< SDLSimulation >( "Depth Buffer", crimild::alloc< Settings >( argc, argv ) );
+    crimild::init();
+    crimild::vulkan::init();
 
-	auto scene = crimild::alloc< Group >();
+    Log::setLevel( Log::Level::LOG_LEVEL_ALL );
 
-	OBJLoader loader( FileSystem::getInstance().pathForResource( "assets/models/sponza/sponza.obj" ) );
-	auto model = loader.load();
-	if ( model != nullptr ) {
-		scene->attachNode( model );
-	}
+    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Depth Buffer", crimild::alloc< Settings >( argc, argv ) );
 
-	auto camera = crimild::alloc< Camera >( 45.0f, 4.0f / 3.0f, 1.0f, 3000.0f );
-	camera->local().setTranslate( 0.0f, 200.0f, 400.0f );
-	camera->local().rotate().fromEulerAngles( 0.0f, -0.5f * Numericf::PI, 0.0f );
-    camera->setRenderGraph( createRenderGraph() );
-	scene->attachNode( camera );
+    SharedPointer< ImageManager > imageManager = crimild::alloc< crimild::stb::ImageManager >();
 
-	sim->setScene( scene );
+    sim->addSystem( crimild::alloc< ExampleVulkanSystem >() );
 
-	return sim->run();
+    return sim->run();
 }
 
