875be8aa3b0847c3894223dc4fc64dc7fb1bea53
Hernan Saez <hhsaez@gmail.com>
2020-03-18 20:15:35 -0300

    Simplify Triangle demo

--- examples/Triangle/Main.cpp
+++ examples/Triangle/Main.cpp
@@ -41,89 +41,60 @@ public:
             return false;
         }
 
-        auto program = ShaderProgramLibrary::getInstance()->get( constants::SHADER_PROGRAM_UNLIT_P2C3_COLOR );
-
-        auto pipeline = [&] {
-            auto pipeline = crimild::alloc< Pipeline >();
-            pipeline->program = crimild::retain( program );
-            return pipeline;
-        }();
-
-        auto vbo = crimild::alloc< VertexP2C3Buffer >(
-            containers::Array< VertexP2C3 > {
-                {
-                    .position = Vector2f( -0.5f, 0.5f ),
-                    .color = RGBColorf( 1.0f, 0.0f, 0.0f ),
-                },
-                {
-                    .position = Vector2f( -0.5f, -0.5f ),
-                    .color = RGBColorf( 0.0f, 1.0f, 0.0f ),
-                },
-                {
-                    .position = Vector2f( 0.5f, -0.5f ),
-                    .color = RGBColorf( 0.0f, 0.0f, 1.0f ),
-                },
-                {
-                    .position = Vector2f( 0.5f, 0.5f ),
-                    .color = RGBColorf( 1.0f, 1.0f, 1.0f ),
-                },
-            }
-        );
-
-        auto ibo = crimild::alloc< IndexUInt32Buffer >(
-            containers::Array< crimild::UInt32 > {
-                0, 1, 2,
-            }
-        );
-
-
-        auto texture = Texture::CHECKERBOARD;
-
-        auto triBuilder = [&]( const Vector3f &position ) {
-            auto node = crimild::alloc< Node >();
-
-            auto renderable = node->attachComponent< RenderStateComponent >();
-            renderable->pipeline = pipeline;
-            renderable->vbo = vbo;
-            renderable->ibo = ibo;
-            renderable->uniforms = {
-                [&] {
-                    auto ubo = crimild::alloc< ModelViewProjectionUniformBuffer >();
-                    ubo->node = crimild::get_ptr( node );
-                    return ubo;
-                }(),
-            };
-            renderable->textures = { texture };
-
-            node->local().setTranslate( position );
-
-            auto startAngle = Random::generate< crimild::Real32 >( 0, Numericf::TWO_PI );
-            auto speed = Random::generate< crimild::Real32 >( -1.0f, 1.0f );
-
-            node->attachComponent< LambdaComponent >(
-                [ startAngle, speed ]( Node *node, const Clock &clock ) {
-                    auto time = clock.getAccumTime();
-                    node->local().rotate().fromAxisAngle( Vector3f::UNIT_Z, startAngle + ( speed * time * -90.0f * Numericf::DEG_TO_RAD ) );
-                }
-            );
-
-            return node;
-        };
-
         m_scene = [&] {
             auto scene = crimild::alloc< Group >();
-            for ( auto x = -5.0f; x <= 5.0f; x += 1.0f ) {
-                for ( auto z = -5.0f; z <= 5.0f; z += 1.0f ) {
-                    scene->attachNode( triBuilder( Vector3f( x, 0.0f, z + ( 0.1f * x / 10.0f ) ) ) );
-                }
-            }
+
+            scene->attachNode([&] {
+                auto node = crimild::alloc< Node >();
+
+                auto renderable = node->attachComponent< RenderStateComponent >();
+                renderable->pipeline = [&] {
+                    auto pipeline = crimild::alloc< Pipeline >();
+                    pipeline->program = [&] {
+                        return crimild::retain(
+                            ShaderProgramLibrary::getInstance()->get(
+                                constants::SHADER_PROGRAM_UNLIT_P2C3_COLOR
+                            )
+                        );
+                    }();
+                    return pipeline;
+                }();
+                renderable->vbo = crimild::alloc< VertexP2C3Buffer >(
+                    containers::Array< VertexP2C3 > {
+                        {
+                            .position = Vector2f( 0.0f, 1.0f ),
+                            .color = RGBColorf( 1.0f, 0.0f, 0.0f ),
+                        },
+                        {
+                            .position = Vector2f( -1.0f, -1.0f ),
+                            .color = RGBColorf( 0.0f, 1.0f, 0.0f ),
+                        },
+                        {
+                            .position = Vector2f( 1.0f, -1.0f ),
+                            .color = RGBColorf( 0.0f, 0.0f, 1.0f ),
+                        },
+                    }
+                );
+                renderable->ibo = crimild::alloc< IndexUInt32Buffer >(
+                    containers::Array< crimild::UInt32 > {
+                        0, 1, 2,
+                    }
+                );
+                renderable->uniforms = {
+                    [&] {
+                        auto ubo = crimild::alloc< ModelViewProjectionUniformBuffer >();
+                        ubo->node = crimild::get_ptr( node );
+                        return ubo;
+                    }(),
+                };
+                renderable->textures = {};
+
+                return node;
+            }());
+
             scene->attachNode([] {
-                auto settings = Simulation::getInstance()->getSettings();
-                auto width = settings->get< crimild::Real32 >( "video.width", 0 );
-                auto height = settings->get< crimild::Real32 >( "video.height", 1 );
-                auto camera = crimild::alloc< Camera >( 45.0f, width / height, 0.1f, 100.0f );
-                camera->local().setTranslate( 5.0f, 10.0f, 10.0f );
-                camera->local().lookAt( Vector3f::ZERO );
+                auto camera = crimild::alloc< Camera >();
+                camera->local().setTranslate( 0.0f, 0.0f, 3.0f );
                 Camera::setMainCamera( camera );
                 return camera;
             }());
@@ -184,7 +155,7 @@ int main( int argc, char **argv )
 
     Log::setLevel( Log::Level::LOG_LEVEL_ALL );
 
-    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Triangles", crimild::alloc< Settings >( argc, argv ) );
+    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Triangle", crimild::alloc< Settings >( argc, argv ) );
     sim->addSystem( crimild::alloc< ExampleVulkanSystem >() );
     return sim->run();
 }
