d15daaab909eb5f6f54a3023f198ce40081833a5
Hernan Saez <hhsaez@gmail.com>
2020-09-20 09:14:23 -0300

    Fix Normalmapping example. Fix normal textures for Sponza model

--- examples/NormalMapping/Main.cpp
+++ examples/NormalMapping/Main.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2013, Hernan Saez
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the <organization> nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,23 +25,17 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <Crimild.hpp>
-#include <Crimild_SDL.hpp>
-
-using namespace crimild;
-using namespace crimild::sdl;
-
+/*
 int main( int argc, char **argv )
 {
 	std::cout << "Usage: "
 			  << "\nPress J to toggle color map"
 		  	  << "\nPress K to toggle specular map"
 		  	  << "\nPress L to toggle normal map" << std::endl;
-	
+
 	auto sim = crimild::alloc< SDLSimulation >( "A simple example", crimild::alloc< Settings >( argc, argv ) );
 
 	float vertices[] = {
-        /* Positions */         /* Normals */       /* Tangents */      /* UVs */
 		-2.0f, +2.0f, 0.0f, 	0.0f, 0.0f, 1.0f,	1.0f, 0.0f, 0.0f,	0.0f, 1.0f,
 		-2.0f, -2.0f, 0.0f, 	0.0f, 0.0f, 1.0f,	1.0f, 0.0f, 0.0f, 	0.0f, 0.0f,
 		+2.0f, -2.0f, 0.0, 		0.0f, 0.0f, 1.0f,	1.0f, 0.0f, 0.0f,	1.0f, 0.0f,
@@ -54,7 +48,7 @@ int main( int argc, char **argv )
 
 	auto vbo = crimild::alloc< VertexBufferObject >( VertexFormat::VF_P3_N3_TG3_UV2, 4, vertices );
 	auto ibo = crimild::alloc< IndexBufferObject >( 6, indices );
-	
+
 	auto primitive = crimild::alloc< Primitive >( Primitive::Type::TRIANGLES );
 	primitive->setVertexBuffer( vbo );
 	primitive->setIndexBuffer( ibo );
@@ -70,7 +64,7 @@ int main( int argc, char **argv )
 	auto normalMap = crimild::retain( AssetManager::getInstance()->get< Texture >( "stone-normal.tga" ) );
 	material->setNormalMap( normalMap );
 	geometry->getComponent< MaterialComponent >()->attachMaterial( material );
-	
+
 	auto scene = crimild::alloc< Group >();
 	scene->attachNode( geometry );
 
@@ -98,7 +92,7 @@ int main( int argc, char **argv )
 		if ( Input::getInstance()->isKeyDown( 'K' ) ) {
 			material->setSpecularMap( material->getSpecularMap() != nullptr ? nullptr : specularMap );
 		}
-		
+
 		if ( Input::getInstance()->isKeyDown( 'L' ) ) {
 			material->setNormalMap( material->getNormalMap() != nullptr ? nullptr : normalMap );
 		}
@@ -139,4 +133,184 @@ int main( int argc, char **argv )
 	sim->setScene( scene );
 	return sim->run();
 }
+*/
 
+#include <Crimild.hpp>
+#include <Crimild_GLFW.hpp>
+#include <Crimild_STB.hpp>
+#include <Crimild_Vulkan.hpp>
+
+using namespace crimild;
+using namespace crimild::glfw;
+
+class ExampleVulkanSystem : public GLFWVulkanSystem {
+public:
+    crimild::Bool start( void ) override
+    {
+        if ( !GLFWVulkanSystem::start() ) {
+            return false;
+        }
+
+        m_frameGraph = crimild::alloc< FrameGraph >();
+
+        m_scene = [ & ] {
+            auto scene = crimild::alloc< Group >();
+
+            scene->attachNode(
+                [] {
+                    auto geometry = crimild::alloc< Geometry >();
+                    geometry->attachPrimitive( crimild::alloc< BoxPrimitive >( BoxPrimitive::Params { .invertFaces = true } ) );
+                    geometry->attachComponent< MaterialComponent >()->attachMaterial(
+                        [] {
+                            auto loadTexture = []( auto fileName ) {
+                                auto texture = crimild::alloc< Texture >();
+                                texture->imageView = [&] {
+                                    auto imageView = crimild::alloc< ImageView >();
+                                    imageView->image = [&] {
+                                        return ImageManager::getInstance()->loadImage(
+                                            {
+                                                .filePath = {
+                                                    .path = fileName,
+                                                },
+                                            }
+                                        );
+                                    }();
+                                    return imageView;
+                                }();
+                                texture->sampler = [&] {
+                                    auto sampler = crimild::alloc< Sampler >();
+                                    return sampler;
+                                }();
+                                return texture;
+                            };
+
+                            auto material = crimild::alloc< SimpleLitMaterial >();
+                            material->setDiffuseMap( loadTexture( "stone-color.tga" ) );
+                        	material->setSpecularMap( loadTexture( "stone-specular.tga" ) );
+                            material->setNormalMap( loadTexture( "stone-normal.tga" ) );
+                            return material;
+                        }() );
+                	geometry->attachComponent< RotationComponent >( Vector3f::ONE.getNormalized(), 0.01 );
+                    return geometry;
+                }() );
+
+            scene->attachNode(
+                [] {
+                    auto group = crimild::alloc< Group >();
+                    group->attachNode(
+                        [] {
+                            auto geometry = crimild::alloc< Geometry >();
+                            geometry->attachPrimitive(
+                                crimild::alloc< SpherePrimitive >(
+                                    SpherePrimitive::Params {
+                                        .type = Primitive::Type::TRIANGLES,
+                                        .layout = VertexP3N3TC2::getLayout(),
+                                        .radius = 0.05f,
+                                    }
+                                )
+                            );
+                            geometry->attachComponent< MaterialComponent >()->attachMaterial(
+                                [] {
+                                    auto material = crimild::alloc< UnlitMaterial >();
+                                    material->setColor( RGBAColorf::ONE );
+                                    return material;
+                                }()
+                            );
+                            return geometry;
+                        }()
+                    );
+                    group->attachNode(
+                        [] {
+                            auto light = crimild::alloc< Light >(
+                                Light::Type::POINT
+                            );
+                            light->setAttenuation( Vector3f( 1.0f, 0.7f, 0.8f ) );
+                            return light;
+                        }()
+                    );
+                    group->attachComponent< LambdaComponent >(
+                        [ t = 0.0f ] ( auto node, auto &clock ) mutable {
+                            auto speed = 0.005f;
+                            auto x = Numericf::remap( -1.0f, 1.0f, -0.95f, 0.95f, Numericf::cos( t ) * Numericf::sin( t ) );
+                            auto y = Numericf::remapSin( -0.95f, 0.95f, t );
+                            auto z = Numericf::remapCos( -0.95f, 0.95f, t );;
+                            if ( !Input::getInstance()->isKeyDown( CRIMILD_INPUT_KEY_SPACE ) ) {
+                                node->local().setTranslate( x, y, z );
+                                t += speed + clock.getDeltaTime();
+                            }
+                        }
+                    );
+                    return group;
+                }()
+            );
+
+            scene->attachNode(
+                [ & ] {
+                    auto camera = crimild::alloc< Camera >();
+                    camera->local().setTranslate( 0.0f, 0.0f, 3.0f );
+                    camera->local().lookAt( Vector3f::ZERO );
+                    return camera;
+                }() );
+
+            scene->perform( StartComponents() );
+
+            return scene;
+        }();
+
+        m_composition = [ & ] {
+            using namespace crimild::compositions;
+            return present( renderScene( m_scene ) );
+        }();
+
+        if ( m_frameGraph->compile() ) {
+            auto commands = m_frameGraph->recordCommands();
+            setCommandBuffers( { commands } );
+        }
+
+        return true;
+    }
+
+    void update( void ) override
+    {
+        auto clock = Simulation::getInstance()->getSimulationClock();
+
+        auto updateScene = [ & ]( auto &scene ) {
+            scene->perform( UpdateComponents( clock ) );
+            scene->perform( UpdateWorldState() );
+        };
+
+        updateScene( m_scene );
+
+        GLFWVulkanSystem::update();
+    }
+
+    void stop( void ) override
+    {
+        if ( auto renderDevice = getRenderDevice() ) {
+            renderDevice->waitIdle();
+        }
+
+        GLFWVulkanSystem::stop();
+    }
+
+private:
+    SharedPointer< FrameGraph > m_frameGraph;
+    SharedPointer< Node > m_scene;
+    compositions::Composition m_composition;
+};
+
+int main( int argc, char **argv )
+{
+    crimild::init();
+    crimild::vulkan::init();
+
+    Log::setLevel( Log::Level::LOG_LEVEL_ALL );
+
+    CRIMILD_SIMULATION_LIFETIME auto sim = crimild::alloc< GLSimulation >( "Normal Mapping", crimild::alloc< Settings >( argc, argv ) );
+
+    SharedPointer< ImageManager > imageManager = crimild::alloc< crimild::stb::ImageManager >();
+
+    sim->addSystem( crimild::alloc< ExampleVulkanSystem >() );
+
+    return sim->run();
+}
